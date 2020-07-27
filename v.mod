cb60eeb8bd9b67b5a78aa7bb1d7c4206a1374019
Nikita Vershinin <nikita.s.vershinin@gmail.com>
2020-07-27 10:51:11 +0200

    Renaming is_ipv4 to is_ip_v4 and is_ipv6 to is_ip_v6

--- v.mod
+++ v.mod
@@ -1,6 +1,6 @@
 Module {
 	name: 'validate',
 	description: 'A simple library to validate strings in V language',
-	version: '0.0.14',
+	version: '0.0.15',
 	dependencies: []
 }
--- validators/ip.v
+++ validators/ip.v
@@ -16,18 +16,18 @@ const (
 
 // checks if the string is an IP address (v4 or v6)
 pub fn is_ip(ip string) bool {
-	return is_ipv4(ip) || is_ipv6(ip)
+	return is_ip_v4(ip) || is_ip_v6(ip)
 }
 
 // checks if the string is an IPv4 address
-pub fn is_ipv4(ip string) bool {
+pub fn is_ip_v4(ip string) bool {
 	buf := byteptr(sizeof_v4_address)
 
 	return 1 == C.inet_pton(C.AF_INET, ip.str, &buf)
 }
 
 // checks if the string is an IPv6 address
-pub fn is_ipv6(ip string) bool {
+pub fn is_ip_v6(ip string) bool {
 	buf := byteptr(sizeof_v6_address)
 
 	return 1 == C.inet_pton(C.AF_INET6, ip.str, &buf)
