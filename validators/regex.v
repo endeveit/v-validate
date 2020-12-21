15a07322d425b32f1453b5cc8807b6821be37c05
Nikita Vershinin <nikita.s.vershinin@gmail.com>
2020-12-21 16:00:43 +0100

    Updating the regex validator to the latest V version

--- validators/regex.v
+++ validators/regex.v
@@ -4,15 +4,19 @@ import regex
 
 // Checks if regular expression syntax is correct
 pub fn is_regex_valid(re_query string) bool {
-	mut re := regex.new_regex()
-	re_err, _ := re.compile(re_query)
+	regex.regex_opt(re_query) or {
+		return false
+	}
 
-	return re_err == regex.compile_ok
+	return true
 }
 
 // Checks if the string matches the regular expression
 pub fn is_regex_match(val string, re_query CommonType) bool {
-	mut re, _, _ := regex.regex(re_query.str())
+	mut re := regex.regex_opt(re_query.str()) or {
+		return false
+	}
+
 	start, _ := re.match_string(val)
 
 	return start != regex.no_match_found
